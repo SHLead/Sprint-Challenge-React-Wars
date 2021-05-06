a7b0d179f307852c407abe003445bb6842264ed2
elijahdaniel <e.paminsan@gmail.com>
2021-05-06 14:49:29 -0700

    grabbing data with axios

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function Character() {
+  const [char, setChar] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then(res => setChar(res.data.results))
+  }, [])
+
+  return (
+    <>
+      <h1>Test</h1>
+      {char.map((item, index) => {
+        return (
+          <div className='card' key={index}>
+            <h2>{item.name}</h2>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Character
